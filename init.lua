c6d8d31ec67ca890028f3d952526e420f53ba0d5
CjiW <cao_jingwei@qq.com>
2023-07-27 10:38:38 +0800

    first commit

---
+++ init.lua
@@ -0,0 +1,19 @@
+-- load options
+require('options')        
+
+
+-- load keymappings
+require('keymaps')
+
+
+-- load Packervim
+require('plugins')
+
+
+-- Set colorscheme
+require('colorscheme')
+
+
+-- Set LSP
+require('lsp')
+
